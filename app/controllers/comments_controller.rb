269ef56ccbfcbbd1da5364504a7f3355b464b164
hoshin <66007415+tarutaros@users.noreply.github.com>
2021-09-04 17:29:44 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+
+  # def index
+  #   @comment = Comment.all
+  # end
+
+  def create
+    @comment = Comment.new(comment_params)
+    # @comment.user_id = current_user.id
+    if @comment.save
+       redirect_to "/prototypes/#{@comment.prototype_id}"
+    else
+      @prototype = @comment.prototype
+      # @comments = @prototype.comments
+      render "prototypes/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    # @prototypes = @user.prototypes
+    # @user = @user.name
+    # @profile = @user.profile
+    # @occupation = @user.occupation
+    # @position = @user.position
+  end
+
+end
